f07370655b432eee05be96036db17f253669de93
Ali-hassan-yousaf <alihassan.tech27@gmail.com>
2025-06-23 08:19:45 +0500

    Update Lostandfound.jsx

--- src/pages/Lostandfound.jsx
+++ src/pages/Lostandfound.jsx
@@ -4,7 +4,7 @@ const PetFinder = () => {
   return (
     <div style={{ margin: 0, overflow: 'hidden' }}>
       <iframe
-        src="https://cc9ccd35368989c2e0.gradio.live/"  
+        src=" https://e2a97dbb6b52bec215.gradio.live"  
         title="Pet Finder"
         style={{
           border: 'none',
